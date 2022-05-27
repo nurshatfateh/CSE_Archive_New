dfc5f5b90af2a9dcd682015d9d862dfdcdfc59ba
Tanvin420 <tanvinmdjamaluddintanvin@hotmail.com>
2022-05-28 04:13:27 +0600

    New login and registration page added. Minor bug fix in student and teacher registration form .

--- js/login.js
+++ js/login.js
@@ -1,10 +1,11 @@
+/*eslint-env es6*/
 const adminEmail = "admin@email.com";
 const adminPass = "admin";
 
 function loginValidator() {
-  const email = document.getElementById("emailInput").value;
-  const pass = document.getElementById("passwordInput").value;
-  if (email === adminEmail && pass === adminPass) {
+const email = document.getElementById("emailInput").value;
+const pass = document.getElementById("passwordInput").value;
+  if (email === adminEmail && pass === adminPass ) {
     window.location.href = "home.html";
   }
 }
