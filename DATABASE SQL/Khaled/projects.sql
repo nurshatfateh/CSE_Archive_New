8c653232f6113c0c615814964e8d3eb1cac5e53b
Khaled Hasan Irfan <khaledirfan7@gmail.com>
2022-06-28 01:10:08 +0600

    sql projects updated

--- DATABASE SQL/Khaled/projects.sql
+++ DATABASE SQL/Khaled/projects.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2022 at 09:03 PM
+-- Generation Time: Jun 27, 2022 at 09:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -33,13 +33,13 @@ CREATE TABLE `projects` (
   `NoofStudent` int(10) NOT NULL,
   `StudentID` varchar(255) NOT NULL,
   `Supervisors` varchar(255) NOT NULL,
-  `Member-1Name` varchar(255) NOT NULL,
-  `Member-2Name` varchar(255) NOT NULL,
-  `Member-3Name` varchar(255) NOT NULL,
-  `Member-4Name` varchar(255) NOT NULL,
-  `Member-5Name` varchar(255) NOT NULL,
-  `CommenceDate` date NOT NULL,
-  `CompletionDate` date NOT NULL,
+  `Member1Name` varchar(255) NOT NULL,
+  `Member2Name` varchar(255) NOT NULL,
+  `Member3Name` varchar(255) NOT NULL,
+  `Member4Name` varchar(255) NOT NULL,
+  `Member5Name` varchar(255) NOT NULL,
+  `CommenceDate` varchar(255) NOT NULL,
+  `CompletionDate` varchar(255) NOT NULL,
   `ProjectTitle` varchar(255) NOT NULL,
   `ProjectDomain` varchar(255) NOT NULL,
   `ProjectSynopsis` varchar(255) NOT NULL,
@@ -51,15 +51,17 @@ CREATE TABLE `projects` (
   `GithubLink` varchar(1000) NOT NULL,
   `ProjectFile` varchar(255) NOT NULL,
   `RelevantProjects` varchar(1000) NOT NULL,
-  `ProjectID` int(255) NOT NULL
+  `ProjectID` int(255) NOT NULL,
+  `ProjectType` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`ProjectName`, `Academicyear`, `NoofStudent`, `StudentID`, `Supervisors`, `Member-1Name`, `Member-2Name`, `Member-3Name`, `Member-4Name`, `Member-5Name`, `CommenceDate`, `CompletionDate`, `ProjectTitle`, `ProjectDomain`, `ProjectSynopsis`, `TeamPhoto`, `Proposal`, `Prototype`, `Budget`, `URLofFrontend`, `GithubLink`, `ProjectFile`, `RelevantProjects`, `ProjectID`) VALUES
-('Mist archive', '2021-2022', 4, '202014016,202014019,202014033,202014040', 'lec nafiz, lec muhaimin, lec adeeb', 'Tanvin Jamal', 'Khaled Hasan ', 'Md Rashid Ul Islam', 'Nurshat Fateh Ali', '', '2022-06-01', '2022-06-16', 'Data archive cse', 'whole cse dept', 'Archive', '', 'Hello archive', 'yes', '200000', 'http / archive ', 'github repo', 'mistdataarchive', 'dbms', 1);
+INSERT INTO `projects` (`ProjectName`, `Academicyear`, `NoofStudent`, `StudentID`, `Supervisors`, `Member1Name`, `Member2Name`, `Member3Name`, `Member4Name`, `Member5Name`, `CommenceDate`, `CompletionDate`, `ProjectTitle`, `ProjectDomain`, `ProjectSynopsis`, `TeamPhoto`, `Proposal`, `Prototype`, `Budget`, `URLofFrontend`, `GithubLink`, `ProjectFile`, `RelevantProjects`, `ProjectID`, `ProjectType`) VALUES
+('Mist archive', '2021-2022', 4, '202014016,202014019,202014033,202014040', 'lec nafiz, lec muhaimin, lec adeeb', 'Easin Arafat', 'Khaled Hasan ', 'Md Rashid Ul Islam', 'Nurshat Fateh Ali', '', '2022-06-01', '2022-06-16', 'Data archive cse', 'whole cse dept', 'Archive', '', 'Hello archive', 'yes', '200000', 'http / archive ', 'github repo', 'mistdataarchive', 'dbms', 1, 'Studentproject'),
+('CYBER RANGE', '2021-2022', 5, '201814005', 'Lec Shadman Aadeeb', 'NURSHAT', 'NAIEM', 'RASHID', 'TANVIN', 'KHALED', '15/06/2022', '22/06/2022', 'PENETRATION', 'CYBER', 'SECURITY', '', '', '', '1234', 'WWW.CYBERRANGE.COM', 'REPO', 'blockchain10.jpg', 'ML', 3, 'StudentProject');
 
 --
 -- Indexes for dumped tables
@@ -79,7 +81,7 @@ ALTER TABLE `projects`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `ProjectID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ProjectID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
