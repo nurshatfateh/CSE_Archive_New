3af6549ae31000960d0b8274353cf12c0307c15f
Tanvin420 <tanvinmdjamaluddintanvin@hotmail.com>
2022-07-27 01:07:56 +0600

    Achivement and Activity pagination full

--- Admin_page/achievements_admin.php
+++ Admin_page/achievements_admin.php
@@ -264,6 +264,56 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
           $results_per_page = 3;
 
+
+
+          if (isset($_GET['search']) && $_GET['search'] == 1) {
+            $year = $_GET['year'];
+            $number_of_pages = $_GET['nopage'];
+            $page = $_GET['page'];
+            $start = NULL;
+            $end = NULL;
+            $key = NULL;
+            goto ss1;
+          } else if (isset($_GET['search']) && $_GET['search'] == 2) {
+            $start = $_GET['start'];
+            $end = $_GET['end'];
+            $number_of_pages = $_GET['nopage'];
+            $page = $_GET['page'];
+            $key = NULL;
+            $year = null;
+            goto  ss2;
+          } else if (isset($_GET['search']) && $_GET['search'] == 2) {
+            if (isset($_GET['start'])) {
+              $start = $_GET['start'];
+            } else {
+              $start = NULL;
+            }
+            if (isset($_GET['end'])) {
+              $end = $_GET['end'];
+            } else {
+              $end = NULL;
+            }
+
+            if (isset($_GET['key'])) {
+              $key = $_GET['key'];
+            } else {
+              $key = NULL;
+            }
+            $year = NULL;
+
+            $number_of_pages = $_GET['nopage'];
+            $page = $_GET['page'];
+            goto ss3;
+          } else {
+            $search = 0;
+            $page = 1;
+            $year = NULL;
+            $start = NULL;
+            $end = NULL;
+            $key = NULL;
+          }
+
+
           $result = mysqli_query($conn, "SELECT * FROM addachievement");
 
 
@@ -277,12 +327,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             $page = $_GET['page'];
           }
 
+
           $this_page_first_result = ($page - 1) * $results_per_page;
 
-
-
           $sql = 'SELECT * FROM addachievement  order by date desc LIMIT ' . $this_page_first_result . ',' .  $results_per_page;
-
 
           if (isset($_POST['year'])) {
             $year = $_POST['year'];
@@ -299,33 +347,56 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
           if (isset($_POST['search'])) {
             if (isset($_POST['year'])) {
-             
-              $sql1 = 'SELECT * FROM addachievement where Academic_year = "' . $year .
-                '" order by date desc ';
-                
-                $results_per_page = 3;
-                $resultkey = mysqli_query($conn, $sql1);
-                $number_of_results = mysqli_num_rows($resultkey);
-                $number_of_pages = ceil($number_of_results / $results_per_page);
+              $sql = "SELECT * FROM addachievement where Academic_year = '" . $year .
+                "' order by date desc ";
+
+              $resultkey = mysqli_query($conn, $sql);
+              $number_of_results = mysqli_num_rows($resultkey);
+              $number_of_pages = ceil($number_of_results / $results_per_page);
+
+              ss1:
+
+              if (!isset($_GET['page'])) {
+                $page = 1;
+              } else {
+                $page = $_GET['page'];
+              }
 
 
-                $sql = $sql1;
+              $this_page_first_result = ($page - 1) * $results_per_page;
 
+              $sql = "SELECT * FROM addachievement where Academic_year = '" . $year .
+                "' order by date desc LIMIT " . $this_page_first_result . "," .  $results_per_page;
+
+
+              $search = 1;
             } else if (isset($_POST['start']) && isset($_POST['end']) && ($start != '1970-01-01' || $end != '1970-01-01')) {
-              
+
               //echo $start. "  ";
               // echo $end;
-               $sql2 = "SELECT * FROM addachievement where date >= '"  . $start .
+              $sql = "SELECT * FROM addachievement where date >= '"  . $start .
                 "' AND  date <=' " . $end . " ' order by date desc ";
-                
-                $results_per_page = 3;
-                $resultkey = mysqli_query($conn, $sql2);
-                $number_of_results = mysqli_num_rows($resultkey);
-                $number_of_pages = ceil($number_of_results / $results_per_page);
-                
 
-                $sql = $sql2;
+              $resultkey = mysqli_query($conn, $sql);
+              $number_of_results = mysqli_num_rows($resultkey);
+              $number_of_pages = ceil($number_of_results / $results_per_page);
 
+              ss2:
+
+              if (!isset($_GET['page'])) {
+                $page = 1;
+              } else {
+                $page = $_GET['page'];
+              }
+
+              $this_page_first_result = ($page - 1) * $results_per_page;
+
+              $sql = "SELECT * FROM addachievement where date >= '"  . $start .
+                "' AND  date <=' " . $end . " ' order by date desc LIMIT " . $this_page_first_result . ',' .  $results_per_page;
+
+
+
+              $search = 2;
             }
             /*else if ( isset($_POST['key']) || ($start =='1970-01-01' || $end =='1970-01-01')) {
             $number_of_pages = 1;
@@ -333,25 +404,39 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             $sql =  $sql = "SELECT * FROM addachievement where AchievementTitle LIKE '%"  .$key.
             "%' OR  Achievement_detailes LIKE '%" .$key. "%' OR Organizer LIKE '%" .$key. "%' OR TeamName like '%" .$key. "%' order by date desc ";
           } */ else if (isset($_POST['key']) || !($start == '1970-01-01' || $end == '1970-01-01')) {
-              
+
               //echo $key;
               //echo $start. "  ";
               //echo $end;
-               $sql3 = "SELECT * FROM addachievement where AchievementTitle LIKE '%"  . $key .
+              $sql = "SELECT * FROM addachievement where AchievementTitle LIKE '%"  . $key .
                 "%' OR  Achievement_detailes LIKE '%" . $key . "%' OR Organizer LIKE '%" . $key . "%' OR TeamName like '%" . $key . "%' 
-            AND date >= '" . $start . "' AND  date <=' " . $end . " 'order by date desc ";
-            
-            $results_per_page = 3;
-            $resultkey = mysqli_query($conn, $sql3);
-            $number_of_results = mysqli_num_rows($resultkey);
-            $number_of_pages = ceil($number_of_results / $results_per_page);
+                AND date >= '" . $start . "' AND  date <=' " . $end . " 'order by date desc ";
 
-            $sql = $sql3;
+              $resultkey = mysqli_query($conn, $sql);
+              $number_of_results = mysqli_num_rows($resultkey);
+              $number_of_pages = ceil($number_of_results / $results_per_page);
 
+              ss3:
+
+              if (!isset($_GET['page'])) {
+                $page = 1;
+              } else {
+                $page = $_GET['page'];
+              }
+
+              $this_page_first_result = ($page - 1) * $results_per_page;
+
+              $sql = "SELECT * FROM addachievement where AchievementTitle LIKE '%"  . $key .
+                "%' OR  Achievement_detailes LIKE '%" . $key . "%' OR Organizer LIKE '%" . $key . "%' OR TeamName like '%" . $key . "%' 
+              AND date >= '" . $start . "' AND  date <=' " . $end . " 'order by date desc LIMIT " . $this_page_first_result . ',' .    $results_per_page;
+
+
+              $search = 3;
             }
           }
 
           xx:
+
 
 
           $result = mysqli_query($conn, $sql);
@@ -383,7 +468,9 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
                     <p class="card-text fw-normal text-wrap">
                       <?php echo substr($row["Achievement_detailes"], 0, 150); ?>
                     </p>
-                    <a href="achievments_details_admin.php?id=<?php echo $row["id"]; ?>">View More</a>
+                    <div class="text-center">
+                      <a class="btn btn-primary btn-md" role="button" aria-disabled="true" href="achievments_details_admin.php?id=<?php echo $row["id"]; ?>">View More</a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -403,9 +490,9 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <?php
             for ($i = 1; $i <= $number_of_pages; $i++) {
               if ($i == $page)
-                echo '<li class="page-item active"> <a class="page-link" href="achievements_admin.php?page=' . $i . '">'  . $i . '</a> </li>';
+                echo '<li class="page-item active"> <a class="page-link" href="achievements_admin.php?page=' . $i . '&search=' . $search . '&year=' . $year . '&start=' . $start . '&end=' . $end . '&key=' . $key . '&nopage=' . $number_of_pages . '">'  . $i . '</a> </li>';
               else
-                echo '<li class="page-item"> <a class="page-link" href="achievements_admin.php?page=' . $i . '">'  . $i . '</a> </li>';
+                echo '<li class="page-item"> <a class="page-link" href="achievements_admin.php?page=' . $i . '&search=' . $search . '&year=' . $year . '&start=' . $start . '&end=' . $end . '&key=' . $key . '&nopage=' . $number_of_pages . '">'  . $i . '</a> </li>';
             }
             ?>
             <li class="page-item"> <a href=""></a> </li>
